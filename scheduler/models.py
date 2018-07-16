022bd0b445bfa9d9fe59415143c4316169a0c742
jagger <jagger909@gmail.com>
2018-07-16 10:54:53 +0300

    Доработал правильное отображение временных окон на сегодняшнюю дату в заявке.

--- scheduler/models.py
+++ scheduler/models.py
@@ -3,24 +3,6 @@ from generic.choice_enum import ChoiceEnum
 from enum import Enum
 # Create your models here.
 
-
-# class SchedulerManager(models.Manager):
-# def create_scheduler(self):
-# pass
-#        scheduler = self.create(title=title)
-# do something with the book
-#       return book
-
-# TIME_CHOICES = (
-#     ('t1', '8:00-10:00'),
-#     ('t2', '10:00-12:00'),
-#     ('t3', '12:00-14:00'),
-#     ('t4', '14:00-16:00'),
-#     ('t5', '16:00-18:00'),
-#     ('t6', '18:00-20:00'),
-#     ('t7', '20:00-22:00'),
-# )
-
 class Scheduler(models.Model):
 
     class TimeChoice(ChoiceEnum):
@@ -41,6 +23,5 @@ class Scheduler(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     sched_un_id = models.CharField(db_index= True, unique=True, max_length=10)
 
-    # object = SchedulerManager()
     def __str__(self):
         return self.address
--- scheduler/views.py
+++ scheduler/views.py
@@ -37,22 +37,35 @@ class SchedulerView(CreateView):
     form_class = SchedulerForm
     template_name = 'scheduler/scheduler2.html'
 
-
+TIME_INDEX = {
+    '08:00': 'T1',
+    '10:00':'T2',
+    '12:00': 'T3',
+    '14:00': 'T4',
+    '16:00': 'T5',
+    '18:00': 'T6',
+    # '20:00': 'T7',
+}
 def scheduler_free_time(request):
 
     rep_date = datetime.date.today()
 
     # rep_date = datetime.datetime.strptime('09-06-2018', "%d-%m-%Y")
-    num_days = 15
+    num_days = 10
     filtered_sched = Scheduler.objects.filter(repair_date__range=(rep_date, rep_date + datetime.timedelta(days=num_days)))
-
     date_list = [rep_date + datetime.timedelta(days=x) for x in range(0, num_days)]
     events = {}
     for date in date_list:
         busy_time = []
         date_time_list = {choices.name: choices.value for choices in Scheduler.TimeChoice}
-        date_busy_time = filtered_sched.filter(repair_date=date)
+        # Уберем прошедшие окна на сегодняшний день.
+        if date == datetime.date.today():
+            time_now = datetime.datetime.now().time().strftime('%H:%M')
+            for time_window in TIME_INDEX:
+                if time_window < time_now:
+                    date_time_list.pop(TIME_INDEX[time_window])
 
+        date_busy_time = filtered_sched.filter(repair_date=date)
         for time in date_busy_time:
             busy_time.append(time.repair_time)
 
@@ -93,8 +106,7 @@ def scheduler_add(request):
         if filtered_sched:
             return JsonResponse({"response": "Дата уже занята.", 'result': 'error'})
 
-        # char_list = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
-        # clear_telephone = "".join([i for i in post_telephone if i in char_list])
+
 
         sched = Scheduler(username=post_username,
                           address=post_address,
